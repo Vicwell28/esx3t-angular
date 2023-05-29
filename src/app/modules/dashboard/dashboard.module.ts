a7c35275ee91b11665e518d21b5546a305e93bf2
Vicwell28 <vicwell.bj@gmail.com>
2023-05-29 17:17:03 -0600

    updated dashboard

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { DashboardComponent } from './main/dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { MaterialModule } from 'src/app/shared/components/material/material.app.module';
+import { ExampleComponent } from './example/example.component';
 
 @NgModule({
-  declarations: [DashboardComponent],
-  imports: [BrowserModule, DashboardRoutingModule],
+  declarations: [DashboardComponent, ExampleComponent],
+  imports: [BrowserModule, DashboardRoutingModule, MaterialModule],
   providers: [],
   bootstrap: [],
 })
--- src/app/modules/dashboard/main/dashboard.component.ts
+++ src/app/modules/dashboard/main/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LocalStorageService } from 'src/app/core/services/local-storage.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+
+  constructor(private localStorageService: LocalStorageService, private router: Router){}
+
+  profile() {
+
+  }
+
+  singOut() {
+    console.log("sign-out")
+    this.localStorageService.removeItem('token')
+    this.localStorageService.removeItem('role')
+    this.router.navigate(['/sign-in'])
+  }
 
 }
